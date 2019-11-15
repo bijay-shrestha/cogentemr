89dbb119a6dd4bf728a124f7c844fe2270513013
Bijay Shrestha <bijay.shrestha@f1soft.com>
2019-11-15 16:14:28 +0545

    Create storybook

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # cogentemr
+
 Fresh Workspace built with  Lerna, Yarn workspace and React Workspace for Cogent Health
+
+
+#Create new module
+
+```$xslt
+$ npx create-react-app --scripts-version @react-workspaces/react-scripts new-module
+```
---
+++ packages/apps/admin/src/App.js
@@ -0,0 +1,57 @@
+import React, {Component} from 'react';
+import ReactLogo from './ReactLogo.svg';
+import YarnCat from './YarnCat.svg';
+import './App.css';
+
+import {CButton, CInputText} from '@cogentemr/ui-components';
+
+class App extends Component {
+  render() {
+    return (
+        <div className="App">
+          <header className="App-header">
+            <div>
+              <img
+                  src={ReactLogo}
+                  className="React-logo"
+                  alt="React Logo"
+              />
+              <img
+                  src={YarnCat}
+                  className="Yarn-cat"
+                  alt="Yarn Workspaces Cat"
+              />
+            </div>
+            <h1>
+              <a
+                  className="App-link"
+                  href="https://github.com/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+              >
+                <strong>CogentEMR</strong> @ Github
+              </a>
+            </h1>
+            <h2>Hot Reload Your Workspaces</h2>
+            <p className="body">
+              <code className="file">
+                packages/apps/admin-module/src/App.js
+              </code>
+              <code className="file">
+                packages/ui-components/src/CButton/CButton.js
+              </code>
+              <code className="file">
+                packages/ui-components/src/CInputText/CInputText.js
+              </code>
+            </p>
+            <div className="components">
+              <CButton />
+              <CInputText />
+            </div>
+          </header>
+        </div>
+    );
+  }
+}
+
+export default App;
